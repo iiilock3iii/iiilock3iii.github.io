b8b60c492178fb9335f6142ceaef28c843d261da
Collin Welker <iiilock3iii@users.noreply.github.com>
2016-02-24 16:35:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-var express = require('express');
-var app = express();
+var http = require('http')
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/app/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('app/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"})
+  response.end("Hello World\n")
+}).listen(process.env.PORT)
