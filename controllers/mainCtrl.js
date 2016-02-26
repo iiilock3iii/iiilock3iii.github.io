0b9917184bbd74828e3978fd658bd48c040662be
Collin Welker <collinwelker3@gmail.com>
2016-02-26 13:52:15 -0700

    Changed title

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -2,7 +2,7 @@ app.controller('mainCtrl', function ($scope, $http) {
 	
 //	Copyright (C) 2015 Collin Welker
 	
-    $scope.title = "Collin Welker";//$routeParams.urlVariable;
+    $scope.title = "Software Engineer | Portfolio and Resume of Collin Welker";//$routeParams.urlVariable;
 //	console.log($scope.title);
 	
 });
